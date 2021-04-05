7e79e3a23ac99c2f869ea7d69653e31e22140250
Gabriel Borges <gbdesigns13@gmail.com>
2021-04-05 01:03:08 -0300

    Fixed image width 🚀💜

--- src/components/Cities/City.tsx
+++ src/components/Cities/City.tsx
@@ -3,7 +3,7 @@ import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
 export default function City({name, country, flag, image}) {
  return (
   <Box borderRadius="4px" overflow="hidden">
-    <Image src={image} alt={`${name}, ${country}`} h="170px"/>
+    <Image src={image} alt={`${name}, ${country}`} h="170px" w="100%"/>
     <Flex p="6" align="center" justify="space-between" bg="white" border="1px" borderColor="yellow.300" borderTop="0">
       <Flex direction="column">
         <Heading fontSize="xl" fontWeight="500">{name}</Heading>
